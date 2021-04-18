331c2fd1b03f9ed7f93259c6f35a8c4bb1b59aa0
DiegoMutre <tysonmutre10@gmail.com>
2021-04-17 22:33:30 -0500

    Add class when home is observed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 const storyBtn = document.querySelectorAll(".story-btn");
+const homeSection = document.querySelector("#home");
+const scrollUpBtn = document.querySelector(".scroll-up-btn");
 
 function toggleStory(e) {
     e.target.classList.toggle("change");
 }
 
 storyBtn.forEach(btn => btn.addEventListener("click", toggleStory));
+
+function observeHomeSection(entries) {
+    if (entries[0].isIntersecting) {
+        scrollUpBtn.classList.remove("show");
+        return;
+    }
+
+    scrollUpBtn.classList.add("show");
+}
+
+const observer = new IntersectionObserver(observeHomeSection).observe(
+    homeSection
+);
